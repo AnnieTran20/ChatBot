7c0dd63c2e395efbe15ca6003189f12717f63ac7
Annie <ngoc.tran@hope.edu>
2023-03-08 14:39:15 -0500

    Build docs_x docs_y and labels list

--- main.py
+++ main.py
@@ -14,10 +14,24 @@ with open("intents.json") as file:
 words = []
 labels = []
 docs = []
-
+docs_x = []
+docs_y = []
+#intent = dict
 for intent in data["intents"]:
+    #pattern: question -string
     for pattern in intent["patterns"]: 
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
+        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
+    if intent["tags"] not in labels:
+        labels.append(intent["tag"])
+words = [setmmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+labels = sorted(labels)
+
+training = []
+output = []
 
 
